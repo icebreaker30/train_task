70a66cde2fbbe946dbfa86e74a8a3b0b2731c94f
icebreaker30 <40492646+icebreaker30@users.noreply.github.com>
2022-04-04 19:46:25 +0300

    +1 задача

---
+++ PODGOTOVKA_permutation in string.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def checkInclusion(self, s1, s2):
+        w, matched = len(s1), 0
+        cntr = dict()
+        for i in s1:
+            cntr[i] = cntr.get(i, 0) + 1
+        for i in range(len(s2)):
+            if s2[i] in cntr:
+                cntr[s2[i]] -= 1
+                if cntr[s2[i]] == 0:
+                    matched += 1
+            if i >= w and s2[ i -w] in cntr:
+                if cntr[s2[ i -w]] == 0:
+                    matched -= 1
+                cntr[s2[ i -w]] += 1
+
+            if matched == len(cntr):
+                return True
+
+        return False
