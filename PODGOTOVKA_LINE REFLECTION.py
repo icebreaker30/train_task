216e5b134677689670b093ae862c7e47538674f3
icebreaker30 <40492646+icebreaker30@users.noreply.github.com>
2022-04-08 17:24:19 +0300

    +1 задача - line reflection

--- PODGOTOVKA_LINE REFLECTION.py
+++ PODGOTOVKA_LINE REFLECTION.py
@@ -1,56 +1,26 @@
 
 points = [[1,1],[-1,1]]
-class Solution2(object):
+
+class Solution3(object):
     def isReflected(self, points):
         """
-        :type points: List[List[int]]
-        :rtype: bool
+        Найти наибольшую и наименьшую горизонтальную координату всех точек, взять центр магазина в качестве оси симметрии
+                 Для каждой точки посмотрите, находится ли осесимметричная точка в точках
+                 Обратите внимание, что значение dict является набором
         """
-        if not points:
-            return True
-        points.sort()
-        # Space: O(n)
-        points[len(points)/2:] = sorted(points[len(points)//2:], key=lambda x, y: y[1] - x[1] if x[0] == y[0] else x[0] - y[0])
-        mid = points[0][0] + points[-1][0]
-        left, right = 0, len(points) - 1
-        while left <= right:
-            if (mid != points[left][0] + points[right][0]) or \
-               (points[left][0] != points[right][0] and \
-                points[left][1] != points[right][1]):
+        from collections import defaultdict
+        dictionary, max_x, min_x = defaultdict(set), -float('inf'), float('inf')
+        for x, y in points:
+            max_x = max(max_x, x)
+            min_x = min(min_x, x)
+            dictionary[x].add(y)
+
+        mid = (min_x + max_x) / 2
+
+        print(mid)
+        for x, y in points:
+            if y  not in dictionary[2*mid - x]:
                 return False
-            left += 1
-            right -= 1
         return True
-
-
-class Solution:
-    def isReflected(self, points):
-        """
-        :type points: List[List[int]]
-        :rtype: bool
-        """
-        import sys
-        if len(points) == 0: return True
-        s, s_removed = set([]), set([])
-        min_x, max_x = sys.maxsize, -sys.maxsize-1
-        for [x, y] in points:
-            if x<min_x: min_x = x
-            if x>max_x: max_x = x
-        mirror_x = (min_x+max_x)/2
-
-        for [x, y] in points:
-            if x == mirror_x: continue
-            if (x, y) in s_removed: continue
-            if (2*mirror_x-x, y) in s:
-                s.remove((2*mirror_x-x, y))
-                s_removed.add((2*mirror_x-x, y))
-                s_removed.add((x, y))
-            else:
-                s.add((x, y))
-        return len(s) == 0
-
-reflect = Solution()
+reflect = Solution3()
 print(reflect.isReflected(points))
-points.sort()
-print(points)
-print(points[1:])
