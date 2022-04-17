e80691b05132ab32f822a7a2d5dc90c80a7691cb
icebreaker30 <40492646+icebreaker30@users.noreply.github.com>
2022-04-17 15:10:48 +0300

    исправил maximize distance to closest person

--- PODGOTOVKA_maximize distance to closest person.py
+++ PODGOTOVKA_maximize distance to closest person.py
@@ -1,11 +1,31 @@
-prev_seat = None
-dist = float("-inf")
+class Solution:
+    def maxDistToClosest(self, seats):
+        """
+        :type seats: List[int]
+        :rtype: int
+        """
+        prev = None
+        res = -float('inf')
+        for i in range(len(seats)):
+            if seats[i] == 1:
+                if prev == None:
+                    res = i
+                    prev = i
+                else:
+                    res = max(res, (i-prev) // 2)
+                prev = i
+        res = max(res, len(seats)-1-prev)
+        return res
+# We iterate i over range(len(seats)), and initialize prev = None, which records the index of the previous 1, and the
+# solution of the problem res = -float('inf'). If seats[i] == 1, and prev == None, it means that we encounter
+# the first seated person (e.g., we meet the 1 in such configuration [0,0,1,....]),
+# then the current largest distance is i, which can be achieved by sitting at the leftmost seat,
+# and we update res = i, prev = i. If seats[i] == 1, and prev != None,
+# it means that we encounter at least two persons already (e.g.,
+# we meet the second 1 in such configuration [...,1,0,0,0,1,...]), the by sitting in the middle of the i and prev,
+# we maximize the minimum distance between the nearest person, which is (i-prev) // 2, we update res = max(res, (i-prev) // 2), and prev = i.
+# Finally, after iterating i over range(len(seats)), we also need to consider the possibility of sitting at the rightmost seat (e.g., in such configuration [...,1,0,0,0]),
+# and we need to make a final update of res by res = max(res, len(seats)-1-prev). Then we return res.
 
-for indx in range (len(seats)):
-    if seats[indx] == 1:
-        if prev_seat == None:
-            dist = indx
-        else:
-            dist = max(dist, (indx - prev_seat))
-dist max(dist, len(seats) - 1 - prev_seat)
-return dist
+#Time complexity: O(n), space complexity: O(1).
+
