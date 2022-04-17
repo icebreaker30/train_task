5943b7e20f67b260302fd90939b92f984431085a
icebreaker30 <40492646+icebreaker30@users.noreply.github.com>
2022-04-17 15:15:46 +0300

    исправил symmetric tree

--- PODGOTOVKA_symmetric tree.py
+++ PODGOTOVKA_symmetric tree.py
@@ -1,9 +1,23 @@
-def dfs(leftnode,rightnode):
-    if (leftnode is None and rightnode is None):
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root == None:
+            return True
+        stack = [root,root]
+        while stack:
+            r1 = stack.pop()
+            r2 = stack.pop()
+            if r1 == None and r2 == None:
+                continue
+            if r1 == None or r2 == None:
+                return False
+            if r1.val != r2.val:
+                return False
+            stack.append(r1.left)
+            stack.append(r2.right)
+            stack.append(r1.right)
+            stack.append(r2.left)
         return True
-    if (leftnode is None or rightnode is None):
-        return False
-    if leftnode.val != rightnode.val:
-        return False
-    return df(leftnode.left, rightnode.right) and df(leftnode.right,rightnode.left)
-return dfs(root.left, root.right)
