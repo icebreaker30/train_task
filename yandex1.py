ae9c6058a91a2e3c96f5784973c9f0ea66760f26
icebreaker30 <40492646+icebreaker30@users.noreply.github.com>
2022-04-19 11:38:14 +0300

    FIBONACI

--- yandex1.py
+++ yandex1.py
@@ -3,10 +3,15 @@ d = min(a[1], a[2])
 e = max(a[1], a[2])
 dis = e - d - 1
 dis2 = a[0] - e + d - 1
-f = min(dis, dis2)
+print(min(dis, dis2))
 
-if f >= 0:
-	print(f)
-else:
-	print(0)
-	
+list1 = list(map(int, input().split()))
+
+min1 = min(list1[1],list[2])
+max1 = max(list[1],list[2])
+
+dist1 = max1-min1 -1
+dist2= list1[0] + min1 - max1 -1
+
+
+print(min(dist1,dist2))
